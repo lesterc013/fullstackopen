13b54644962d2e608def6e13d1e2be9b0ecee972
Lester Chan <lesterc013@gmail.com>
2024-06-17 20:09:31 +0800

    2.9 Filter implemented

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -5,10 +5,28 @@ const App = () => {
     { 
       name: 'Arto Hellas',
       number: "040-1234567"
-    }
+    },
+    { 
+      name: 'Lester Chan',
+      number: "93219818"
+    },
+    { 
+      name: 'Tom Cat',
+      number: "098765432"
+    },
+    { 
+      name: 'Jerry Lee Kwok Song Bo',
+      number: "00999999000"
+    },
+    { 
+      name: 'Big Ray Wong',
+      number: "092092309"
+    },
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  // const [newFilter, setNewFilter] = useState('') Don't need this because like one of the past examples, the newFilter is always "one step behind" hence can't use this to filter -- or rather no point since we won't need to access it again
+  const [filteredPersons, setFilteredPersons] = useState(persons)
 
   const handleNameChange = event => setNewName(event.target.value)
   
@@ -25,11 +43,18 @@ const App = () => {
       }))
     }
   }
+
+  const handleFilter = (event) => {
+    const filter = event.target.value.toLowerCase()
+    setFilteredPersons(persons.filter(person => person.name.toLowerCase().includes(filter)))
+  }
   
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      <div>filter shown with a <input onChange={handleFilter} /></div>
       <form onSubmit={handleSubmit} >
+        <h2>add a new</h2>
         <div>
           name: <input onChange={handleNameChange} />
         </div>
@@ -39,7 +64,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <div key={person.name}>{person.name} {person.number}</div>)}
+      {filteredPersons.map(person => <div key={person.name}>{person.name} {person.number}</div>)}
     </div>
   )
 }
