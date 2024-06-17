2872e68c8a87d2b2416996ce4c91ec1b8e99191f
Lester Chan <lesterc013@gmail.com>
2024-06-17 15:57:35 +0800

    Import Total component

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,29 +1,40 @@
-import Course from "./components/Course.jsx"
+import Course from "./components/Course.jsx";
+import Total from "./components/Total.jsx";
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+    const course = {
+        id: 1,
+        name: "Half Stack application development",
+        parts: [
+            {
+                name: "Fundamentals of React",
+                exercises: 10,
+                id: 1,
+            },
+            {
+                name: "Using props to pass data",
+                exercises: 7,
+                id: 2,
+            },
+            {
+                name: "State of a component",
+                exercises: 14,
+                id: 3,
+            },
+            {
+                name: "Redux",
+                exercises: 11,
+                id: 4,
+            },
+        ],
+    };
 
-  return <Course course={course} />
-}
+    return (
+        <>
+            <Course course={course} />
+            <Total course={course} />
+        </>
+    );
+};
 
-export default App
+export default App;
