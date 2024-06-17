29458c32efb56d6c3093452c2fc034b2b0e414ac
Lester Chan <lesterc013@gmail.com>
2024-06-17 15:37:30 +0800

    Implemented Course component

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,10 @@
+const Course = ({ course }) => {
+    return (
+        <>
+            <h1>{course.name}</h1>
+            {course.parts.map(part => <p key={part.id}>{part.name} {part.exercises}</p>)}
+        </>
+    )
+}
+
+export default Course
