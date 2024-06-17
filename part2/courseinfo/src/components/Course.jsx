b637e5edf50440d9dec5cc6ee2f4a5317485ca7c
Lester Chan <lesterc013@gmail.com>
2024-06-17 16:23:56 +0800

    Handles arbitrary number of courses

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,10 +1,22 @@
 const Course = ({ course }) => {
+    const initial = 0;
+    const total = course.parts.reduce(
+        (accumulator, part) => accumulator + part.exercises,
+        initial
+    );
     return (
         <>
-            <h1>{course.name}</h1>
-            {course.parts.map(part => <p key={part.id}>{part.name} {part.exercises}</p>)}
+            <h2>{course.name}</h2>
+            {course.parts.map((part) => (
+                <p key={part.id}>
+                    {part.name} {part.exercises}
+                </p>
+            ))}
+            <p>
+                <b>total of {total} exercises</b>
+            </p>
         </>
-    )
-}
+    );
+};
 
-export default Course
+export default Course;
