166b267ceeb72b44211c17fb6606000778ed50d4
Lester Chan <lesterc013@gmail.com>
2024-06-19 15:14:46 +0800

    GET all country names from api

--- part2/data-for-countries/src/App.jsx
+++ part2/data-for-countries/src/App.jsx
@@ -1,4 +1,5 @@
-import { useEffect } from "react"
+import { useState, useEffect } from "react"
+import axios from 'axios'
 
 const Search = ({handleChange}) => {
   return (
@@ -7,14 +8,37 @@ const Search = ({handleChange}) => {
 }
 
 const App = () => {
+  // const [searchValue, setSearchValue] = useState(null)
+  // Testing
+  const [searchValue, setSearchValue] = useState('')
+  const [countriesNames, setCountriesNames] = useState(null)
 
-  const handleChange = (event) => {
-    console.log(event.target.value)
+  // setCountries initial state with the names of all countries in the 'all' endpoint
+  useEffect(() => {
+    console.log('useEffect initiated after initial render')
+    axios
+      .get(allURL)
+      .then(response => response.data)
+      .then(allCountries => {
+        setCountriesNames(allCountries.map(countries => countries.name.common))
+      })
+  }, [])
+
+  // Testing search functionality
+  if (countriesNames) {
+    console.log(countriesNames[0])
+  }
+
+  const allURL = 'https://studies.cs.helsinki.fi/restcountries/api/all'
+  const specificURL = 'https://studies.cs.helsinki.fi/restcountries/api/name/'
+
+  const handleSearch = (event) => {
+    setSearchValue(event.target.value)
   }
 
   return (
     <>
-      <Search handleChange={handleChange} />
+      <Search handleChange={handleSearch} />
     </>
   )
 }
