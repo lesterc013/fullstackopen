4d1850170b787ed4205753cff0123e025c954bf2
Lester Chan <lesterc013@gmail.com>
2024-06-19 17:19:36 +0800

    2.18 Working implementation

--- part2/data-for-countries/src/App.jsx
+++ part2/data-for-countries/src/App.jsx
@@ -7,11 +7,74 @@ const Search = ({handleChange}) => {
   )
 }
 
+const Display = ({ countriesNames, searchValue }) => {
+  const filteredCountries = countriesNames.filter(countryName => countryName.toLowerCase().includes(searchValue))
+
+  if (searchValue === '') {
+    return null
+  }
+  else if (filteredCountries.length > 10) {
+    return <div>Too many matches, specify another filer</div>
+  }
+  else if (filteredCountries.length == 1) {
+    // Call GET on the specific URL
+    // Could probably do this as a specific component since this is gettingg large
+    return <Specific specificCountryName={filteredCountries} />
+  }
+  else {
+    return (
+        filteredCountries.map(countryName => <Country key={countryName} countryName={countryName} />
+      )
+    )
+  }
+}
+
+const Specific = ({specificCountryName}) => {
+  const [specificCountry, setSpecificCountry] = useState(null)
+  const specificURL = 'https://studies.cs.helsinki.fi/restcountries/api/name/'
+  // Without useEffect, we will keep re-rendering the programme. This is because of setSpecificCountry below where everytime a state is set, it causes a re-render
+  // useEffect should only run if there is a change to specificCountryName
+  useEffect(() => {
+    axios
+      .get(`${specificURL}${specificCountryName}`)
+      .then(response => response.data)
+      .then(country => setSpecificCountry(country))
+    }, [specificCountryName])
+
+    if (!specificCountry) {
+      return null
+    } 
+
+    const languages = []
+    for (const [v] of Object.entries(specificCountry.languages)) {
+      languages.push(v)      
+    }
+    console.log(languages)
+    return (
+    <>
+      <h1>{specificCountry.name.common}</h1>
+      <p>Capital: {specificCountry.capital}</p>
+      <p>Area: {specificCountry.area}</p>
+      <h2>Languages</h2>
+      <ul>
+        {languages.map(language => <li key={language}>{language}</li>)}
+      </ul>
+      <img src={specificCountry.flags.png} alt={`${specificCountry.name.common} flag`} />
+    </>
+  )
+}
+
+const Country = ({countryName}) => {
+  return <div>{countryName}</div>
+}
+
 const App = () => {
   // const [searchValue, setSearchValue] = useState(null)
   // Testing
-  const [searchValue, setSearchValue] = useState('')
+  const [searchValue, setSearchValue] = useState(null)
   const [countriesNames, setCountriesNames] = useState(null)
+  
+  const allURL = 'https://studies.cs.helsinki.fi/restcountries/api/all'
 
   // setCountries initial state with the names of all countries in the 'all' endpoint
   useEffect(() => {
@@ -24,21 +87,23 @@ const App = () => {
       })
   }, [])
 
-  // Testing search functionality
-  if (countriesNames) {
-    console.log(countriesNames[0])
-  }
-
-  const allURL = 'https://studies.cs.helsinki.fi/restcountries/api/all'
-  const specificURL = 'https://studies.cs.helsinki.fi/restcountries/api/name/'
-
   const handleSearch = (event) => {
     setSearchValue(event.target.value)
   }
 
+  // If countriesNames null
+  if (!countriesNames) {
+    return (
+      <>
+        find countries <Search handleChange={handleSearch} />
+      </>
+    )
+  }
+
   return (
     <>
-      <Search handleChange={handleSearch} />
+      find countries <Search handleChange={handleSearch} />
+      <Display countriesNames={countriesNames} searchValue={searchValue} />
     </>
   )
 }
