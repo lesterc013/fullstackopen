a20b1b9f0ccf5729dd259408e2da6629a16d914d
Lester Chan <lesterc013@gmail.com>
2024-06-19 21:08:39 +0800

    Update App to import components as required

--- part2/data-for-countries/src/App.jsx
+++ part2/data-for-countries/src/App.jsx
@@ -1,106 +1,7 @@
 import { useState, useEffect } from "react"
 import axios from 'axios'
-
-const Search = ({handleChange}) => {
-  return (
-    <input onChange={handleChange} />
-  )
-}
-
-const Display = ({ countriesNames, searchValue, handleShow }) => {
-  
-  if (searchValue === '' || searchValue === null) {
-    return null
-  }
-
-  const filteredCountries = countriesNames.filter(countryName => countryName.toLowerCase().includes(searchValue.toLowerCase()))
-
-  if (filteredCountries.length > 10) {
-    return <div>Too many matches, specify another filer</div>
-  }
-  else if (filteredCountries.length == 1) {
-    return <Specific specificCountryName={filteredCountries} />
-  }
-  else {
-    return (
-        filteredCountries.map(countryName => <Country key={countryName} countryName={countryName} handleShow={handleShow} />
-      )
-    )
-  }
-}
-
-const Specific = ({specificCountryName}) => {
-  console.log("Rendering...")
-  const [specificCountry, setSpecificCountry] = useState(null)
-  const specificURL = 'https://studies.cs.helsinki.fi/restcountries/api/name/'
-  
-  // Without useEffect, we will keep re-rendering the programme. This is because of setSpecificCountry below where everytime a state is set, it causes a re-render
-  // useEffect should only run if there is a change to specificCountryName
-  useEffect(() => {
-    console.log("useEffect to set specific country")
-    axios
-    .get(`${specificURL}${specificCountryName}`)
-    .then(response => response.data)
-    .then(country => setSpecificCountry(country))
-  }, [specificCountryName])
-
-    if (!specificCountry) {
-      return null
-    } 
-
-    const languages = []
-    for (const [k, v] of Object.entries(specificCountry.languages)) {
-      languages.push(v)      
-    }
-
-    return (
-    <>
-      <h1>{specificCountry.name.common}</h1>
-      <p>Capital: {specificCountry.capital}</p>
-      <p>Area: {specificCountry.area}</p>
-      <h2>Languages</h2>
-      <ul>
-        {languages.map(language => <li key={language}>{language}</li>)}
-      </ul>
-      <img src={specificCountry.flags.png} alt={`${specificCountry.name.common} flag`} />
-      <Weather lat={specificCountry.capitalInfo.latlng[0]} lon={specificCountry.capitalInfo.latlng[1]} capital={specificCountry.capital} />
-    </>
-  )
-}
-
-const Weather = ({ lat, lon, capital }) => {
-  const [weatherDetails, setWeatherDetails] = useState(null)
-  const apiKey = import.meta.env.VITE_KEY
-  const weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`
-
-  useEffect(() => {
-    axios
-      .get(weatherURL)
-      .then(response => response.data)
-      .then(weather => setWeatherDetails(weather))
-  }, [lat, lon, weatherURL])
-
-  if (!weatherDetails) {
-    return null
-  }
-
-  return (
-    <>
-      <h2>{`Weather in ${capital}`}</h2>
-      <div>Temperature: {weatherDetails.main.temp} C</div>
-      <img src={`https://openweathermap.org/img/wn/${weatherDetails.weather[0].icon}@2x.png`} alt='weather icon' />
-      <div>Wind: {weatherDetails.wind.speed} m/s</div>
-    </>
-  )
-}
-
-const Country = ({ countryName, handleShow }) => {
-  return (
-    <div>
-      {countryName} <button onClick={() => handleShow(countryName)} >show</button>
-    </div>
-  ) 
-}
+import Search from "./components/Search"
+import Display from "./components/Display"
 
 const App = () => {
   const [searchValue, setSearchValue] = useState(null)
@@ -124,14 +25,9 @@ const App = () => {
   }
 
   const handleShow = (countryName) => {
-    // I have the countryName
-    // If i setShow, then I can pass it to Display component
-    // To then filter the countriesNames entire list
-    // Which will cause length to be 1 and hence call Specific
     setSearchValue(countryName)
   }
 
-  // If countriesNames null
   if (!countriesNames) {
     return (
       <>
